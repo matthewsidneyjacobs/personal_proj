c7ce658e12fc4223105f9b7689d6464d2114dc1c
matthewsidneyjacobs <matthewsidneyjacobs@gmail.com>
2017-01-06 22:14:43 -0700

    added login

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var PORT = process.env.PORT || 3000
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
+var bcrypt = require('bcryptjs');
 
 var middleware = require('./middleware.js');
 
@@ -193,7 +194,20 @@ app.post('/users', function (req, res) {
 });
 
 
-db.sequelize.sync().then(function() {
+//POST /users/login
+app.post('/users/login', function(req,res) {
+  var body = _.pick(req.body, "email" , "password");
+
+  db.user.authenticate(body).then(function(user) {
+    res.json(user.toPublicJSON());
+  }, function(e) {
+    res.status(401).send();
+  })
+
+
+});
+
+db.sequelize.sync({force:true}).then(function() {
   app.listen(PORT, function() {
     console.log('I\'m listening on port: ', PORT)
   })
