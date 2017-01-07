82c8ac4f69d20e273af7c4a8faf22e2f682a7cf0
matthewsidneyjacobs <matthewsidneyjacobs@gmail.com>
2017-01-06 18:16:16 -0700

    adding postgres for production

--- db.js
+++ db.js
@@ -1,10 +1,19 @@
 //load all modules into sequelize and then return that db connection to server.js which is going to call that file. server js requests db from db.js, db.js is going to return it so server.js can use it in api
 
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize(undefined, undefined, undefined, {
-  'dialect': 'sqlite',
-  'storage': __dirname+'/data/dev-food-api.sqlite'
-});
+var env = process.env.NODE_ENV || 'development';
+var sequelize;
+
+if (env === 'production') {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres'
+  });
+} else {
+  sequelize = new Sequelize(undefined, undefined, undefined, {
+    'dialect': 'sqlite',
+    'storage': __dirname+'/data/dev-food-api.sqlite'
+  });
+}
 
 var db = {};
 
